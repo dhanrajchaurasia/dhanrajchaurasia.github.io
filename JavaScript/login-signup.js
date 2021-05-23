013a59677b4aea226d10d36751ad5234fe1ee4cb
dhanrajchaurasia <mnnitdhanraj@gmail.com>
2021-05-23 13:33:26 +0530

    Updated

--- JavaScript/login-signup.js
+++ JavaScript/login-signup.js
@@ -7,3 +7,13 @@ togglePassword.addEventListener('click', function (e) {
     // toggle the eye slash icon
     this.classList.toggle('fa-eye-slash');
 });
+
+new bootstrap.Popover(document.querySelector('.example-popover'), {
+    container: 'body'
+  })
+  
+  $(document).ready(function() {
+    $(".name-button").click(function(event) {
+      $('.modal-header h5.modal-title').html("New message to " + $(this).attr('data-name'));
+    });
+  })
